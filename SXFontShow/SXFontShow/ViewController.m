1d61b2204fd85a2ac1f09a856e0ebc6bd1f67266
董尚先 <dantesx2012@gmail.com>
2015-08-02 16:14:40 +0800

    暂且可以运行可以展示

--- SXFontShow/SXFontShow/ViewController.m
+++ SXFontShow/SXFontShow/ViewController.m
@@ -8,20 +8,90 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic,retain)UITableView * tableView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _tableView=[[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    _tableView.delegate=self;
+    _tableView.dataSource=self;
+    [self.view addSubview:_tableView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    //字体家族总数
+    return [[UIFont familyNames] count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    //字体家族包括的字体库总数
+    //    return [[UIFont fontNamesForFamilyName:[[UIFont familyNames] objectAtIndex:section] ] count];
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    //字体家族名称
+    return [[UIFont familyNames] objectAtIndex:section];
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
+{
+    [tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:index] atScrollPosition:UITableViewScrollPositionMiddle animated:NO];
+    return index;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+
+{
+    
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    //字体家族名称
+    NSString *familyName= [[UIFont familyNames] objectAtIndex:indexPath.section];
+    
+    //字体家族中的字体库名称
+    NSString *fontName  = [[UIFont fontNamesForFamilyName:[[UIFont familyNames] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
+    
+    //    NSLog(@"%@",fontName);
+    cell.textLabel.font = [UIFont fontWithName:fontName size:18.0f];
+    
+    cell.textLabel.text=@"董铂然DSXNiubility-2015";
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ - %@", familyName, fontName ];
+    
+    return cell;
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    //字体家族名称
+    //    NSString *familyName= [[UIFont familyNames] objectAtIndex:indexPath.section];
+    
+    //字体家族中的字体库名称
+    NSString *fontName  = [[UIFont fontNamesForFamilyName:[[UIFont familyNames] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
+    
+    NSLog(@"%@",fontName);
+    
 }
 
 @end
